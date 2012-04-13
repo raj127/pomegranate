e7a1a4924aa6f8f1861bff34f388d1fae590cb8d
xiaohuimi@gmail.com <xiaohuimi@gmail.com@4f90302f-293a-c686-beb2-77f39230283a>
2012-04-13 06:31:36 +0000

    作业规程编辑功能编写.

---
+++ trunk/projects/bo/src/main/java/com/darkmi/edit/service/TaskChapterManager.java
@@ -0,0 +1,80 @@
+package com.darkmi.edit.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springside.modules.orm.Page;
+import org.springside.modules.orm.PropertyFilter;
+
+import com.darkmi.edit.dao.TaskChapterDao;
+import com.darkmi.entity.task.TaskChapter;
+
+/**
+ * 作业规程任务管理Manager
+ */
+@Component
+@Transactional
+public class TaskChapterManager {
+	private static Logger logger = LoggerFactory.getLogger(TaskChapterManager.class);
+	private TaskChapterDao taskChapterDao;
+
+	/**
+	 * 保存作业规程任务信息
+	 */
+	public void saveTaskChapter(TaskChapter taskChapter) {
+		//taskDao.save(task);
+		logger.debug("保存TaskChapter信息成功.");
+	}
+
+	/**
+	 * 根据主键删除作业规程任务信息
+	 * @param id
+	 */
+	public void deleteTaskChapter(Long id) {
+		taskChapterDao.delete(id);
+		logger.debug("删除taskChapter id={}", id);
+	}
+
+	/**
+	 * 根据主键得到作业规程任务信息
+	 */
+	@Transactional(readOnly = true)
+	public TaskChapter getTaskChapter(Long id) {
+		//return taskDao.get(id);
+		return null;
+	}
+
+	/**
+	 * 得到所有的作业规程任务列表
+	 */
+	public List<TaskChapter> getAllTaskChapter() {
+		return taskChapterDao.getAll();
+	}
+
+	/**
+	 * 根据Id得到作业规程任务信息
+	 * @return
+	 */
+	@Transactional(readOnly = true)
+	public TaskChapter getTaskChapter(long id) {
+		return taskChapterDao.findUniqueBy("id", id);
+	}
+
+	/**
+	 * 根据属性查询任务信息
+	 */
+	@Transactional(readOnly = true)
+	public Page<TaskChapter> searchTaskChapter(Page<TaskChapter> page, List<PropertyFilter> filters) {
+		return taskChapterDao.findPage(page, filters);
+	}
+
+	@Autowired
+	public void setTaskDao(TaskChapterDao taskDao) {
+		this.taskChapterDao = taskDao;
+	}
+
+}
---
+++ trunk/projects/bo/src/main/java/com/darkmi/edit/web/TaskChapterAction.java
@@ -0,0 +1,109 @@
+package com.darkmi.edit.web;
+
+import java.util.List;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springside.modules.orm.Page;
+import org.springside.modules.orm.PropertyFilter;
+import org.springside.modules.utils.web.struts2.Struts2Utils;
+
+import com.darkmi.edit.service.TaskChapterManager;
+import com.darkmi.entity.task.TaskChapter;
+import com.darkmi.util.CrudActionSupport;
+
+@Namespace("/edit")
+@Results({ @Result(name = CrudActionSupport.RELOAD, location = "design.action?page.pageNo=${page.pageNo}&page.orderBy=${page.orderBy}&page.order=${page.order}&page.pageSize=${page.pageSize}", type = "redirect") })
+public class TaskChapterAction extends CrudActionSupport<TaskChapter> {
+	private static final long serialVersionUID = -2907389496513631586L;
+	private Long id;
+	private TaskChapter taskChapter;
+	private TaskChapterManager taskChapterManager;
+	private Page<TaskChapter> page = new Page<TaskChapter>(20);
+
+	@Override
+	protected void prepareModel() throws Exception {
+		if (id != null) {
+			taskChapter = taskChapterManager.getTaskChapter(id);
+		} else {
+			taskChapter = new TaskChapter();
+		}
+	}
+
+	@Override
+	public TaskChapter getModel() {
+		return taskChapter;
+	}
+
+	@Override
+	public String list() throws Exception {
+		List<PropertyFilter> filters = PropertyFilter.buildFromHttpRequest(Struts2Utils.getRequest());
+		//设置默认排序方式
+		if (!page.isOrderBySetted()) {
+			page.setOrderBy("id");
+			page.setOrder(Page.DESC);
+		}
+		page = taskChapterManager.searchTaskChapter(page, filters);
+		return SUCCESS;
+	}
+
+	@Action("/task/chapter")
+	public String viewChapter() {
+		System.out.println("aaa");
+		return SUCCESS;
+	}
+
+	@Override
+	public String input() throws Exception {
+		return INPUT;
+	}
+
+	@Override
+	public String save() throws Exception {
+		//designManager.saveTask(taskChapter);
+		addActionMessage("保存作业规程任务成功！");
+		return RELOAD;
+	}
+
+	@Override
+	public String delete() throws Exception {
+		//taskChapter = designManager.getTask(id);
+		//designManager.deleteTask(id);
+		//dbLogger.info(SpringSecurityUtils.getCurrentUserName() + ":删除" + taskChapter.getTaskName() + "任务！");
+		//addActionMessage("删除作业规任务成功！");
+		return RELOAD;
+	}
+
+	/**
+	 * 支持使用Jquery.validate Ajax检验用户名是否重复.
+	 */
+	public String checkTaskName() {
+		//		HttpServletRequest request = ServletActionContext.getRequest();
+		//		String providerId = request.getParameter("employeeName");
+		//		String oldProviderId = request.getParameter("oldemployeeName");
+		//
+		//		if (designManager.isTaskNameUnique(providerId, oldProviderId)) {
+		//			Struts2Utils.renderText("true");
+		//		} else {
+		//			Struts2Utils.renderText("false");
+		//		}
+		//		//因为直接输出内容而不经过jsp,因此返回null.
+		return null;
+	}
+
+	public Page<TaskChapter> getPage() {
+		return page;
+	}
+
+	@Autowired
+	public void setTaskChapterManager(TaskChapterManager taskChapterManager) {
+		this.taskChapterManager = taskChapterManager;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
