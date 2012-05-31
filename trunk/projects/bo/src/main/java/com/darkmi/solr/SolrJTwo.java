c64209f16aee569978ee2d51fc8b5043ec3405b6
xiaohuimi@gmail.com <xiaohuimi@gmail.com@4f90302f-293a-c686-beb2-77f39230283a>
2012-05-31 07:08:35 +0000

    编写了两个SolrJ的测试程序.

---
+++ trunk/projects/bo/src/main/java/com/darkmi/solr/SolrJTwo.java
@@ -0,0 +1,41 @@
+package com.darkmi.solr;
+
+import java.util.Iterator;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+
+/**
+ * 测试一下使用SolrJ进行查询.
+ * @author DarkMi
+ *
+ */
+public class SolrJTwo {
+	public static void main(String[] args) {		
+		String url = "http://localhost:8983/solr";
+		HttpSolrServer server;
+		try{
+			server = new HttpSolrServer(url);
+			SolrQuery query = new SolrQuery();
+			query.setQuery("description:锄");
+			query.addField("title");
+			query.addField("author");
+			query.addField("description");
+			
+			query.setStart(0);
+			query.setRows(10);
+			
+			QueryResponse response = server.query(query);
+			SolrDocumentList list = response.getResults();
+			for (Iterator<SolrDocument> iterator = list.iterator(); iterator.hasNext();) {
+				SolrDocument solrDocument = (SolrDocument) iterator.next();
+				System.out.println(solrDocument.getFieldValue("title"));
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
